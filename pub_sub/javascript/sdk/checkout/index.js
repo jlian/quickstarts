242d5eda259a1dec0c445b8d71a9b416fcc9a94d
John Lian <jlian@users.noreply.github.com>
2022-09-21 17:50:39 +0000

    Node app works

--- pub_sub/javascript/sdk/checkout/index.js
+++ pub_sub/javascript/sdk/checkout/index.js
@@ -3,19 +3,19 @@ import { DaprClient } from '@dapr/dapr';
 const DAPR_HOST = process.env.DAPR_HOST || "http://localhost";
 const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || "3500";
 const PUBSUB_NAME = "orderpubsub";
-const PUBSUB_TOPIC = "orders";
+const PUBSUB_TOPIC = "temp-measurements";
 
 async function main() {
   const client = new DaprClient(DAPR_HOST, DAPR_HTTP_PORT);
 
-  for(var i = 1; i <= 10; i++) {
-    const order = {orderId:  i};
+  for(var i = 1; i <= 1000000; i++) {
+    const temperature = Math.floor(Math.random() * (50 ));
 
     // Publish an event using Dapr pub/sub
-    await client.pubsub.publish(PUBSUB_NAME, PUBSUB_TOPIC, order);
-    console.log("Published data: " + JSON.stringify(order));
+    await client.pubsub.publish(PUBSUB_NAME, PUBSUB_TOPIC, temperature);
+    console.log("Published data: " + JSON.stringify(temperature));
 
-    await sleep(1000);
+    await sleep(100);
   }
 }
 async function sleep(ms) {
--- pub_sub/javascript/sdk/order-processor/index.js
+++ pub_sub/javascript/sdk/order-processor/index.js
@@ -1,17 +1,43 @@
 import { DaprServer } from '@dapr/dapr';
+import { DaprClient } from '@dapr/dapr';
 
 const DAPR_HOST = process.env.DAPR_HOST || "http://localhost";
 const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || "3501";
 const SERVER_HOST = process.env.SERVER_HOST || "127.0.0.1";
 const SERVER_PORT = process.env.APP_PORT || 5002;
+const PUBSUB_NAME = "orderpubsub";
+const SUB_TOPIC = "temp-measurements";
+const PUB_TOPIC = "temp-alerts";
+// const TEMP_THRESHOLD = process.env.TEMP_THRESHOLD
+const TEMP_THRESHOLD = process.env.TEMP_THRESHOLD || 25;
+
 
 async function main() {
   const server = new DaprServer(SERVER_HOST, SERVER_PORT, DAPR_HOST, DAPR_HTTP_PORT);
+  const client = new DaprClient(DAPR_HOST, DAPR_HTTP_PORT);
 
   // Dapr subscription routes orders topic to this route
-  server.pubsub.subscribe("orderpubsub", "orders", (data) => console.log("Subscriber received: " + JSON.stringify(data)));
+  server.pubsub.subscribe(PUBSUB_NAME, SUB_TOPIC, (data) => {
+    console.log("Subscriber received: " + JSON.stringify(data));
+    var alert = filterMessage(data);
+    if (alert) {
+      client.pubsub.publish(PUBSUB_NAME, PUB_TOPIC, "Alert!");
+      console.log("Published alert")
+    }
+  });
 
   await server.start();
+}
+
+function filterMessage(msg) {
+  var temperature = JSON.stringify(msg);
+  if (temperature > TEMP_THRESHOLD) {
+    console.log(`Temperature ${temperature} exceeds threshold ${TEMP_THRESHOLD}`);
+    var alert = true;
+    return alert;
+  }
+  var alert = false;
+  return alert;
 }
 
 main().catch(e => console.error(e));
