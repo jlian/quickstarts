5bc7ae5e71856027054d0a19dabbc082c9895437
John Lian <jlian@users.noreply.github.com>
2022-09-22 21:24:48 +0000

    Telemetry optimization

--- pub_sub/javascript/sdk/checkout/index.js
+++ pub_sub/javascript/sdk/checkout/index.js
@@ -4,18 +4,19 @@ const DAPR_HOST = process.env.DAPR_HOST || "http://localhost";
 const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT || "3500";
 const PUBSUB_NAME = "orderpubsub";
 const PUBSUB_TOPIC = "temp-measurements";
+const SLEEP_TIMER = process.env.SLEEP_TIMER || "500"
 
 async function main() {
   const client = new DaprClient(DAPR_HOST, DAPR_HTTP_PORT);
 
-  for(var i = 1; i <= 1000000; i++) {
-    const temperature = Math.floor(Math.random() * (50 ));
+  while(true) {
+    const temperature = Math.floor(Math.random() * (50));
 
     // Publish an event using Dapr pub/sub
     await client.pubsub.publish(PUBSUB_NAME, PUBSUB_TOPIC, temperature);
     console.log("Published data: " + JSON.stringify(temperature));
 
-    await sleep(100);
+    await sleep(SLEEP_TIMER);
   }
 }
 async function sleep(ms) {
